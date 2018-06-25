729a4ce6ac520608bf5fbe23481df465283452a9
crisp <criiispq@gmail.com>
2018-06-25 10:57:20 +0200

    a

--- var_monitor/var_monitor.py
+++ var_monitor/var_monitor.py
@@ -211,6 +211,10 @@ class ProcessTreeMonitor():
         print ("Init : ", proc, var_list, kwargs)
         self.parent_proc = proc
         self.kwargs = kwargs
+        print(VAR_MONITOR_DICT[var](var, self) for var in var_list)
+        print (" ")
+
+
         self.monitor_list = [VAR_MONITOR_DICT[var](var, self) for var in var_list]
         self.report_lapse = kwargs.get('report_lapse', REPORT_LAPSE)
         self.check_lapse = kwargs.get('check_lapse', CHECK_LAPSE)
