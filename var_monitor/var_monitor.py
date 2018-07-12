27e8b48195a441fb11f937e180a1f6a3d5566cc3
crispq95 <criiispq@gmail.com>
2018-07-12 13:57:43 +0200

    a

--- var_monitor/var_monitor.py
+++ var_monitor/var_monitor.py
@@ -248,7 +248,8 @@ class ProcessTreeMonitor():
 
     def update_process_tree(self):
         if self.parent_proc.children() :
-            self.process_tree[self.parent_proc] = self.parent_proc.children()
+            if self.parent_proc.children():
+                self.process_tree[self.parent_proc] = self.parent_proc.children()
         old_dict = self.process_tree.copy()
 
         for childs in old_dict.values():
