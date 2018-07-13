7729955cfb45929025bd23609d1269301bcf47bb
crispq95 <criiispq@gmail.com>
2018-07-13 12:05:13 +0200

    a

--- var_monitor/var_monitor.py
+++ var_monitor/var_monitor.py
@@ -144,7 +144,6 @@ class CumulativeVarMonitor(VarMonitor):
 
         cur_val = self.get_process_value(some_process)
         cur_pid = some_process.pid
-        resta = 0
 
         if d_childs:
             #print("The process ", some_process.pid, " has those dead childs : ", d_childs)
@@ -179,9 +178,6 @@ class CumulativeVarMonitor(VarMonitor):
             self.var_value_dict[cur_pid] += cur_val
         else:
             self.var_value_dict[cur_pid] = cur_val
-
-        if resta != 0:
-            self.var_value_dict[cur_pid] -= resta
 
         self.set_value_from_value_dict()
 
