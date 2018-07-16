1449b0def30646a677e9c959100492ecf2163a56
crispq95 <criiispq@gmail.com>
2018-07-16 08:52:12 +0200

    aa

--- var_monitor/var_monitor.py
+++ var_monitor/var_monitor.py
@@ -309,9 +309,9 @@ class ProcessTreeMonitor():
                     if n.children():
                         for child in n.children():
                             if child.is_running():
-                                child_list.append(c)
+                                child_list.append(child)
                             else :
-                                temp_dead_childs.append(c)
+                                temp_dead_childs.append(child)
                         if child_list:
                             l_act[n] = child_list
                             self.process_tree[n] = child_list
