ca1e567d8b39d5d64c0761aac3ba734401f94382
crispq95 <criiispq@gmail.com>
2018-07-16 12:52:54 +0200

    a

--- var_monitor/var_monitor.py
+++ var_monitor/var_monitor.py
@@ -159,7 +159,7 @@ class CumulativeVarMonitor(VarMonitor):
             for c in d_childs:
                 if c.pid in self.var_value_dict:
                     resta += self.var_value_dict[c.pid]
-                    #self.var_value_dict.pop(c.pid)
+                    self.var_value_dict.pop(c.pid)
             #print("from ", some_process.pid ," -- dead childs ",d_childs," value : ",resta)
             self.reset_dead_childs(some_process)
 
@@ -168,7 +168,6 @@ class CumulativeVarMonitor(VarMonitor):
         # if cur_pid in self.var_value_dict and cur_val < self.var_value_dict[cur_pid]:
         self.var_value_dict[cur_pid] = cur_val
         self.var_value_dict[cur_pid] -= resta
-
 
         self.set_value_from_value_dict()
 
